92ec5656a066d4e3f958037cdf48f74147ecd77e
Hasan Malik <hasanimranmalik245@gmail.com>
2026-02-25 04:41:36 -0500

    updated PostgreSQL queries

--- Ripple/schema.sql
+++ Ripple/schema.sql
@@ -16,3 +16,22 @@ create table public.messages(
   created_at timestamptz default now(),
   message_id serial primary key
 );
+
+alter table public.profiles
+add column email text
+
+create or replace function public.handle_new_user()
+returns trigger as
+$$ begin
+  insert into public.profiles(id, email)
+  values (new.id, new.email);
+  return new;
+end $$
+LANGUAGE plpgsql SECURITY DEFINER;
+
+
+create trigger on_auth_user_created
+after insert on auth.users
+for each row execute function public.handle_new_user();
+-- now, it says "for each row", but that just means: "for each row that's being inserted into auth.users"
+-- which is just one row!
