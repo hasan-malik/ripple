e9d46020b78f2f30c9957f0c0d38b70567ce3fc9
Hasan Malik <hasanimranmalik245@gmail.com>
2026-02-24 21:00:56 -0500

    Initial Commit

---
+++ Ripple/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ripple
+//
+//  Created by Hasan Malik on 2026-02-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ripple/RippleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RippleApp.swift
+//  Ripple
+//
+//  Created by Hasan Malik on 2026-02-24.
+//
+
+import SwiftUI
+
+@main
+struct RippleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
