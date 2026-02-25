67f4fa58910b9a9e6c97c74d616236d09d03975c
Hasan Malik <hasanimranmalik245@gmail.com>
2026-02-25 16:56:23 -0500

    realtime messaging listens for messages

--- Ripple/RealtimeMessaging.swift
+++ Ripple/RealtimeMessaging.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Realtime
+import Supabase
 
 
 @Observable
@@ -19,5 +20,38 @@ class RealtimeMessaging {
     
     func startListening() async {
         print("Realtime listening for messages")
+        
+        let channel = supabase.realtimeV2.channel("MyChannel")
+        print("1")
+        let stream = channel.postgresChange(AnyAction.self, schema: "public", table: "messages")
+        // first argument: the type of actions you're listerning for: InsertAction.self, UpdateAction.self, DeleteAction.self, or AnyAction.self
+        
+        print("2")
+        do {
+            print("3")
+            try await channel.subscribeWithError() // start listening to the channel
+            print("4")
+            for await change in stream {
+                // now, <change> has the type of the first argument to postgresChange
+                // so for us, <change> is of type AnyAction
+                // AnyAction is an enum
+                // the .record or .oldRecord attribute is the row of the table
+                print("5: change: \(change)")
+                switch change {
+                case .insert(let change):
+                        print("inserted:", change.record)
+                case .delete(let change):
+                        print("deleted:", change.oldRecord)
+                case .update(let change):
+                        print("updated:", change.record)
+                }
+                print("6")
+                
+            }
+        } catch {
+            print("Realtime messaging error: \(error)")
+        }
+        
+
     }
 }
