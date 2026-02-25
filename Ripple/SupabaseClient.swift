6059ad6c07789cac0130f4ee96296fc0ac8e5c08
Hasan Malik <hasanimranmalik245@gmail.com>
2026-02-25 02:35:56 -0500

    supabase client

---
+++ Ripple/SupabaseClient.swift
@@ -0,0 +1,10 @@
+//
+//  SupabaseClient.swift
+//  Ripple
+//
+//  Created by Hasan Malik on 2026-02-25.
+//
+
+import Foundation
+import Supabase
+let supabase = SupabaseClient(supabaseURL: URL(string: "YOUR_URL")!, supabaseKey: "YOUR_ANON_KEY")
